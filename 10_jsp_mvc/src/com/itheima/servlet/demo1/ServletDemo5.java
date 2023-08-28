1e45829705a61646270b7b2f842967571e4bc3d3
ENKAKOU.YJH <121547866+ENKAKOU@users.noreply.github.com>
2023-08-28 21:25:37 +0900

    Update ServletDemo5.java

--- 10_jsp_mvc/src/com/itheima/servlet/demo1/ServletDemo5.java
+++ 10_jsp_mvc/src/com/itheima/servlet/demo1/ServletDemo5.java
@@ -9,9 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * ServletContext对象作用一：获取web项目的信息
- */
+
+// ServletContext对象作用一：获取web项目的信息
 public class ServletDemo5 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 1.获取文件的MIME的类型：
