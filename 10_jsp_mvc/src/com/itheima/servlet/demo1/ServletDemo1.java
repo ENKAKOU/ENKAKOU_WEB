6eeb3a192dd68cc1977effe36a95da57900e1724
ENKAKOU.YJH <121547866+ENKAKOU@users.noreply.github.com>
2023-08-28 12:50:45 +0900

    Update ServletDemo1.java

--- 10_jsp_mvc/src/com/itheima/servlet/demo1/ServletDemo1.java
+++ 10_jsp_mvc/src/com/itheima/servlet/demo1/ServletDemo1.java
@@ -15,30 +15,22 @@ import javax.servlet.ServletResponse;
 public class ServletDemo1 implements Servlet{
 
 	@Override
-	/**
-	 * Servlet对象被实例化的时候init方法就会执行，而且只执行一次。（Servlet是单例的）
-	 */
+	// Servlet对象被实例化的时候init方法就会执行，而且只执行一次。（Servlet是单例的）
 	public void init(ServletConfig config) throws ServletException {
 		System.out.println("ServletDemo1初始化了......");
 	}
 	@Override
-	/**
-	 * service方法:任何一次请求都会执行service方法。可以执行多次。
-	 */
+
+	// service方法:任何一次请求都会执行service方法。可以执行多次。
 	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
 		System.out.println("ServletDemo1执行了......");
 	}
 	@Override
-	/**
-	 * Servlet从服务器中移除或者服务器关闭的时候销毁Servlet。执行一次。
-	 */
+
+	// Servlet从服务器中移除或者服务器关闭的时候销毁Servlet。执行一次。
 	public void destroy() {
 		System.out.println("ServletDemo1被销毁了.......");
 	}
-	
-	
-	
-	
 	
 	@Override
 	public ServletConfig getServletConfig() {
@@ -46,14 +38,10 @@ public class ServletDemo1 implements Servlet{
 		return null;
 	}
 
-
-
 	@Override
 	public String getServletInfo() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-
 
 }
