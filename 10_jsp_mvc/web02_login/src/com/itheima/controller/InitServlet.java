46ae58aed1aa0004a38cc09e1e59aa9d16d2401e
ENKAKOU.YJH <121547866+ENKAKOU@users.noreply.github.com>
2023-08-28 23:58:45 +0900

    Update InitServlet.java

--- 10_jsp_mvc/web02_login/src/com/itheima/controller/InitServlet.java
+++ 10_jsp_mvc/web02_login/src/com/itheima/controller/InitServlet.java
@@ -6,26 +6,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * 初始化的Servlet
- * * 将这个Servlet配置成启动时加载
- */
+// 初始化的Servlet(将这个Servlet配置成启动时加载)
 public class InitServlet extends HttpServlet {
 	@Override
-	/**
-	 * 初始化的方法:
-	 */
+	// 初始化的方法:
 	public void init() throws ServletException {
-		// 初始一个值为0。
-		int count = 0;
-		// 将这个值存入到ServletContext域中。
-		this.getServletContext().setAttribute("count", count);
+		int count = 0;		// 初始一个值为0。
+		this.getServletContext().setAttribute("count", count);		// 将这个值存入到ServletContext域中。
 	}
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+	throws ServletException, IOException {
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+	throws ServletException, IOException {
 		doGet(request, response);
 	}
 
