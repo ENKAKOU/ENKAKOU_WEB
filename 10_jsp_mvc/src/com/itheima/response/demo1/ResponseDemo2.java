a81d875cf9a54a459a9e3130265549b479471d41
ENKAKOU.YJH <121547866+ENKAKOU@users.noreply.github.com>
2023-08-28 12:49:17 +0900

    Update ResponseDemo2.java

--- 10_jsp_mvc/src/com/itheima/response/demo1/ResponseDemo2.java
+++ 10_jsp_mvc/src/com/itheima/response/demo1/ResponseDemo2.java
@@ -6,9 +6,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * 定向的页面
- */
+
+// 定向的页面
 public class ResponseDemo2 extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
