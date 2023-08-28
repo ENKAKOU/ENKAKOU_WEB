edd4e9cfaf91ab728ad30601e98ddf035f2883f2
ENKAKOU.YJH <121547866+ENKAKOU@users.noreply.github.com>
2023-08-28 23:59:20 +0900

    Update LoginServlet.java

--- 10_jsp_mvc/web02_login/src/com/itheima/controller/LoginServlet.java
+++ 10_jsp_mvc/web02_login/src/com/itheima/controller/LoginServlet.java
@@ -52,7 +52,8 @@ public class LoginServlet extends HttpServlet {
 		}
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+	                                         throws ServletException, IOException {
 		doGet(request, response);
 	}
 
