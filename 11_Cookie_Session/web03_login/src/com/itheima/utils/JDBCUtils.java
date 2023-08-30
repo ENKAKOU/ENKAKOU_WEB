326417fbab7124ea45e39501e3a20d663e896743
ENKAKOU.YJH <121547866+ENKAKOU@users.noreply.github.com>
2023-08-30 15:45:21 +0900

    Update JDBCUtils.java

--- 11_Cookie_Session/web03_login/src/com/itheima/utils/JDBCUtils.java
+++ 11_Cookie_Session/web03_login/src/com/itheima/utils/JDBCUtils.java
@@ -8,16 +8,12 @@ import java.sql.Statement;
 import javax.sql.DataSource;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
-
 /**
  * JDBC的工具类
  * @author jt
- *
  */
 public class JDBCUtils {
-	// 创建一个连接池：但是这个连接池只需要创建一次即可。
-	private static final ComboPooledDataSource dataSource = new ComboPooledDataSource();
-	
+	private static final ComboPooledDataSource dataSource = new ComboPooledDataSource(); 	// 创建一个连接池：但是这个连接池只需要创建一次即可。
 	/**
 	 * 获得连接的方法
 	 * @throws SQLException 
@@ -25,12 +21,8 @@ public class JDBCUtils {
 	public static Connection getConnection() throws SQLException{
 		return dataSource.getConnection();
 	}
-	
-	/**
-	 * 获得连接池:
-	 */
-	public static DataSource getDataSource(){
+
+	public static DataSource getDataSource(){    //获得连接池
 		return dataSource;
 	}
-	
 }
