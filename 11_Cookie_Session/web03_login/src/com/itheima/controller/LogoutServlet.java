61b7da22e2d55ed46e52019f536693bed033c3cc
ENKAKOU.YJH <121547866+ENKAKOU@users.noreply.github.com>
2023-08-30 15:36:23 +0900

    Update LogoutServlet.java

--- 11_Cookie_Session/web03_login/src/com/itheima/controller/LogoutServlet.java
+++ 11_Cookie_Session/web03_login/src/com/itheima/controller/LogoutServlet.java
@@ -6,20 +6,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * 系统退出的Servlet
- */
+// 系统退出的Servlet
 public class LogoutServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// 销毁session：
-		request.getSession().invalidate();
-		// 页面跳转
-		response.sendRedirect("/web03_login/success.jsp");
+
+		request.getSession().invalidate();		// 销毁session：
+		response.sendRedirect("/web03_login/success.jsp");		// 页面跳转
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
-
 }
