e2226d964d4899c983f05e43619735735719e054
ENKAKOU.YJH <121547866+ENKAKOU@users.noreply.github.com>
2023-09-11 22:48:13 +0900

    Update JedisDemo2.java

--- redis/src/jedisTest/JedisDemo2.java
+++ redis/src/jedisTest/JedisDemo2.java
@@ -1,34 +1,30 @@
 package jedisTest;
 
 import java.util.List;
-
 import org.junit.Before;
 import org.junit.Test;
 
 import redis.clients.jedis.Jedis;
 
-//string操作
-public class JedisDemo2 {
+public class JedisDemo2 {      //string操作
 	Jedis jedis;
 
 	@Before
 	public void createJedis() {
 		jedis = new Jedis("192.168.19.128");
-		// 设置密码
-		jedis.auth("admin");
+		jedis.auth("admin");		// 设置密码
 	}
 
-	// 演示 set get
 	@Test
-	public void test1() {
+	public void test1() {    	//set get
 		jedis.set("username", "tom");		
 		String value = jedis.get("username");
 		System.out.println(value);
 	}
 	
-	//演示mset  mget
+
 	@Test
-	public void test2(){
+	public void test2(){     	//mset  mget
 		jedis.mset("password","123","age","20");
 		
 		List<String> values = jedis.mget("username","password","age");
@@ -36,14 +32,12 @@ public class JedisDemo2 {
 		System.out.println(values);
 	}
 	
-	//演示  append  setrange  getrange
+
 	@Test
-	public void test3(){
+	public void test3(){	    //append  setrange  getrange
 		//jedis.append("username"," is boy");
-		
 		//jedis.setrange("username", 7,"girl");
 		System.out.println(jedis.get("username"));
-		
 		
 		System.out.println(jedis.getrange("username", 7, -1));
 	}
