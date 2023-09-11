af6cd1bb766af1420a4a3cc6ec1e8de50cd3ab0e
enkakou <enkakou0716@gmail.com>
2023-09-11 23:05:13 +0900

    no message

--- redis/src/jedisTest/JedisDemo3.java
+++ redis/src/jedisTest/JedisDemo3.java
@@ -1,36 +1,60 @@
-package redis.src.jedisTest;
+package jedisTest;
 
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import redis.clients.jedis.BinaryClient.LIST_POSITION;
+import redis.clients.jedis.Jedis;
+
+//list操作
 public class JedisDemo3 {
+	Jedis jedis;
 
-    Jedis jedis;
+	@Before
+	public void createJedis() {
+		jedis = new Jedis("192.168.19.128");
+		// 设置密码
+		jedis.auth("admin");
+	}
 
-    publice void createJedis(){
-        jedis = new Jedis("193.168.19.218");
+	// 演示lpush lrange
+	@Test
+	public void test1() {
+		jedis.lpush("names", "tom", "james", "张三", "李四");
+		List<String> names = jedis.lrange("names", 0, -1);
+		System.out.println(names);
+	}
 
-        jedis.auth("admin");
-    }
+	// lset
+	@Test
+	public void test2() {
+		// jedis.lset("names", 1, "王五");
+		// List<String> names = jedis.lrange("names", 0, -1);
+		// System.out.println(names);
 
-    public void test1(){
-        jedis.lpush("names","tom","james","张三","李四");
-        List<String> names = jedis.lrange("name",0,-1);
-        System.out.println(names);
-    }
+		String value = jedis.lindex("names", 1);
+		System.out.println(value);
+	}
 
-    public void test2(){
-        String value = jedis.lindex("names",1);
-        System.out.println(value);
-    }
+	// linsert
+	@Test
+	public void test3() {
+		jedis.linsert("names", LIST_POSITION.BEFORE, "james", "fox");
 
-    public void test3(){
-        jedis.linsert("names",LIST_POSITION.BEFORE,"james","fox");
+		List<String> names = jedis.lrange("names", 0, -1);
+		System.out.println(names);
+	}
+	
+	// lrem
+	@Test
+	public void test4(){
+		jedis.lrem("names", 1, "tom");
+		List<String> names = jedis.lrange("names", 0, -1);
+		System.out.println(names);
+		
+		
+	}
 
-        List<String> names = jedis.lrange("names",0,-1);
-        System.out.println(names);
-    }
-
-    public void test4(){
-        jedis.lrem("names",1,"tom");
-        List<String> names = jedis.lrange("name",0,-1);
-        System.out.println(names);
-    }
 }
--- redis/src/jedisTest/JedisDemo4.java
+++ redis/src/jedisTest/JedisDemo4.java
@@ -2,5 +2,5 @@ package redis.src.jedisTest;
 
 public class JedisDemo4 {
 
-    
+
 }
