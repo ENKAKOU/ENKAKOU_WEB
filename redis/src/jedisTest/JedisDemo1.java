30f5b6694a503e1b4b4b838124b18aa10e70f3ef
ENKAKOU.YJH <121547866+ENKAKOU@users.noreply.github.com>
2023-09-11 23:04:44 +0900

    Update JedisDemo1.java

--- redis/src/jedisTest/JedisDemo1.java
+++ redis/src/jedisTest/JedisDemo1.java
@@ -6,9 +6,10 @@ public class JedisDemo1 {
 
 	public static void main(String[] args) {
 		Jedis jedis = new Jedis("192.168.19.128");
-		//设置密码
-		jedis.auth("admin");
+
+		jedis.auth("admin");		//设置密码
 		jedis.set("foo", "bar");
+		
 		String value = jedis.get("foo");
 		System.out.println(value);
 	}
