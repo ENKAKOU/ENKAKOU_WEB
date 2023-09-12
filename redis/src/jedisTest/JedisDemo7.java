fe2b168304ccddf8a3f3bfd16713597356c4066d
ENKAKOU.YJH <121547866+ENKAKOU@users.noreply.github.com>
2023-09-12 11:25:21 +0900

    Update JedisDemo7.java

--- redis/src/jedisTest/JedisDemo7.java
+++ redis/src/jedisTest/JedisDemo7.java
@@ -11,37 +11,37 @@ import org.junit.Test;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.Tuple;
 
-//key的通用操作
-public class JedisDemo7 {
+
+public class JedisDemo7 {	                                  //key的通用操作
 	Jedis jedis;
 
 	@Before
 	public void createJedis() {
 		jedis = new Jedis("192.168.19.128");
-		// 设置密码
-		jedis.auth("admin");
-
+		jedis.auth("admin");	                          // 设置密码
 	}
-	// keys patten
+
+	
 	@Test
-	public void test1(){
+	public void test1(){	                                  //keys patten
 		Set<String> keys = jedis.keys("*");
 		System.out.println(keys);
 	}
 	
-	// del key
+
 	@Test
-	public void test2(){
+	public void test2(){	                                  //del key
 		Long del = jedis.del("user");
 		System.out.println(del);
 	}
 	
-	//关于key时间设置
+
 	@Test
-	public void test3(){
-		//jedis.expire("username", 200); //设置生命周期为200秒
+	public void test3(){	                                  //关于key时间设置
+		
+		//jedis.expire("username", 200);                  //设置生命周期为200秒
 		jedis.persist("username");
-		Long ttl = jedis.ttl("username"); //获取生命周期值
+		Long ttl = jedis.ttl("username");                 //获取生命周期值
 		System.out.println(ttl);
 		
 	}
