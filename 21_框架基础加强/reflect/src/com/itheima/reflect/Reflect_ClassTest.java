f9f590ca1d24905565082b08e4a426f8c582c588
ENKAKOU.YJH <121547866+ENKAKOU@users.noreply.github.com>
2023-09-12 13:39:27 +0900

    Update Reflect_ClassTest.java

--- 21_框架基础加强/reflect/src/com/itheima/reflect/Reflect_ClassTest.java
+++ 21_框架基础加强/reflect/src/com/itheima/reflect/Reflect_ClassTest.java
@@ -2,27 +2,26 @@ package com.itheima.reflect;
 
 import org.junit.Test;
 
-//演示如果获取Class对象
+//如果获取Class对象
 public class Reflect_ClassTest {
 
-    // 1.如果你得到一个类的对象
     @Test
-    public void test1() {
+    public void test1() {                                             // 1.如果你得到一个类的对象
         User user = new User();
-        Class class1 = user.getClass();             // getClass方法是从Object类中继承
+        Class class1 = user.getClass();                               // getClass方法是从Object类中继承
         System.out.println(class1);
     }
 
-    // 2.可以直接通过类名获取
+
     @Test
-    public void test2() {
+    public void test2() {                                             // 2.可以直接通过类名获取
         Class clazz = User.class;
         System.out.println(clazz);
     }
 
-    // 3.推荐使用的方案
+
     @Test
-    public void test3() throws ClassNotFoundException {
+    public void test3() throws ClassNotFoundException {               // 3.推荐使用的方案
         Class clazz=Class.forName("com.itheima.reflect.User");
         System.out.println(clazz);
     }
