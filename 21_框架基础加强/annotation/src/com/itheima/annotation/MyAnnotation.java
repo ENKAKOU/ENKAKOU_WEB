7fcf4b70578daf47b8fcb656ea2ee51d37e89ca8
ENKAKOU.YJH <121547866+ENKAKOU@users.noreply.github.com>
2023-09-14 12:32:44 +0900

    Update MyAnnotation.java

--- 21_框架基础加强/annotation/src/com/itheima/annotation/MyAnnotation.java
+++ 21_框架基础加强/annotation/src/com/itheima/annotation/MyAnnotation.java
@@ -2,14 +2,14 @@ package com.itheima.annotation;
 
 public @interface MyAnnotation {
 
-	public String[] show(); // 在注解中，我们管它叫做注解属性
+	public String[] show();            // 在注解中叫做注解属性
 
-	boolean[] a(); // 基本类型
+	boolean[] a();                     // 基本类型
 
-	MyEnum[] me(); // 枚举类型
+	MyEnum[] me();                     // 枚举类型
 
-	MyAnnotation2[] ma2(); // 注解类型
+	MyAnnotation2[] ma2();             // 注解类型
 
-	Class[] clazz(); //Class类型
+	Class[] clazz();                   // Class类型
 
 }
